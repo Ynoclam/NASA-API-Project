20460950f8dc97b2581e1f7dc4b1cf730473d592
Maksim <bboba8777@gmail.com>
2025-05-02 22:12:23 +0300

    Initial Commit

---
+++ NASA API Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NASA API Project
+//
+//  Created by Максим Кобрянов on 02.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NASA API Project/NASA_API_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASA_API_ProjectApp.swift
+//  NASA API Project
+//
+//  Created by Максим Кобрянов on 02.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct NASA_API_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
